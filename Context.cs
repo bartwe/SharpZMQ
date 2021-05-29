91e33a01f93c572ffdb3fb8fca5eb62c1c87ee0c
Bart van der Werf <bluelive@gmail.com>
2021-05-29 18:12:38 +0200

    Minimal zmq binding for .net5

---
+++ Context.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using SharpZMQ.lib;
+
+namespace SharpZMQ {
+    public struct Context {
+        internal IntPtr _context;
+
+        public static Context Create() {
+            var instance = LibzmqBinding.zmq_ctx_new();
+            if (instance == IntPtr.Zero)
+                LibzmqBinding.RaiseError("Failed to open context.");
+            return new() { _context = instance };
+        }
+
+        public Socket CreateRequestSocket() {
+            Debug.Assert(_context != IntPtr.Zero);
+            var socket = LibzmqBinding.zmq_socket(_context, SocketType.ZMQ_REQ);
+            if (socket == IntPtr.Zero)
+                LibzmqBinding.RaiseError("Failed to create socket.");
+            return new() { _socket = socket };
+        }
+
+        public void Close() {
+            Debug.Assert(_context != IntPtr.Zero);
+            var rc = LibzmqBinding.zmq_ctx_term(_context);
+            if (rc != 0)
+                LibzmqBinding.RaiseError("Failed to close context.");
+            _context = IntPtr.Zero;
+        }
+    }
+}
---
+++ Socket.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using SharpZMQ.lib;
+
+namespace SharpZMQ {
+    public struct Socket {
+        internal IntPtr _socket;
+
+        public void Connect(string address) {
+            Debug.Assert(_socket != IntPtr.Zero);
+            var addressUtf8 = Marshal.StringToCoTaskMemUTF8(address);
+            var rc = LibzmqBinding.zmq_connect(_socket, addressUtf8);
+            Marshal.FreeCoTaskMem(addressUtf8);
+            if (rc != 0)
+                LibzmqBinding.RaiseError("Failed to connect socket to: '" + address + "'.");
+        }
+
+        public void Close() {
+            Debug.Assert(_socket != IntPtr.Zero);
+            var rc = LibzmqBinding.zmq_close(_socket);
+            if (rc != 0)
+                LibzmqBinding.RaiseError("Failed to close socket.");
+            _socket = IntPtr.Zero; ;
+        }
+
+        // on successful send the message is consumed
+        public unsafe bool Send(ref Message message, bool dontWait, bool sendMore) {
+            Debug.Assert(_socket != IntPtr.Zero);
+            Message localMessage = message;
+            Message* msgPtr = &localMessage;
+            var options = (dontWait ? SendRecvOptions.ZMQ_DONTWAIT : SendRecvOptions.ZMQ_NONE) | (sendMore ? SendRecvOptions.ZMQ_SNDMORE : SendRecvOptions.ZMQ_NONE);
+            var rc = LibzmqBinding.zmq_msg_send((IntPtr)msgPtr, _socket, options);
+            if (rc < 0) {
+                if (dontWait) {
+                    var errno = LibzmqBinding.zmq_errno();
+                    if (errno == (int)ErrorCodes.EAGAIN)
+                        return false;
+                }
+                LibzmqBinding.RaiseError("Failed to send message from socket");
+            }
+            message = localMessage;
+            return true;
+        }
+
+        // 'more' is explicitly not exposed as this should be obvious to the protocol above this layer
+        public unsafe bool Receive(ref Message message, bool dontWait) {
+            Debug.Assert(_socket != IntPtr.Zero);
+            Message localMessage = message;
+            Message* msgPtr = &localMessage;
+            var rc = LibzmqBinding.zmq_msg_recv((IntPtr)msgPtr, _socket, dontWait ? SendRecvOptions.ZMQ_DONTWAIT : SendRecvOptions.ZMQ_NONE);
+            if (rc < 0) {
+                if (dontWait) {
+                    var errno = LibzmqBinding.zmq_errno();
+                    if (errno == (int)ErrorCodes.EAGAIN) {
+                        return false;
+                    }
+                }
+                LibzmqBinding.RaiseError("Failed to receive message from socket");
+            }
+            message = localMessage;
+            return true;
+        }
+    }
+}
